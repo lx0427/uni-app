fae1820ca9c59484a57365ad8797828376d73309
xiaofeixiong <1749239438@qq.com>
2019-05-07 16:05:27 +0800

    save

--- unpackage/dist/dev/app-plus/pages/mine/index.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["@charset \x22UTF-8\x22;\n.",[1],"content { text-align: center; height: ",[0,400],"; }\n.",[1],"logo { height: ",[0,200],"; width: ",[0,200],"; margin-top: ",[0,200],"; }\n.",[1],"title { font-size: ",[0,36],"; color: #8f8f94; }\n",],undefined,{path:"./pages/mine/index.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/mine/index.wxml') } }));
